4df2864987c1d439880afd5d3d97db22c2ae6d54
Ivan <99640421+i4k1@users.noreply.github.com>
2023-12-07 11:02:08 +0400

    Update bytebeat_wav.go

--- bytebeat_wav.go
+++ bytebeat_wav.go
@@ -1,4 +1,4 @@
-// 06-12-2023 i4k
+// 07-12-2023 i4k
 // This code creates an output.wav file. You can open it in any media player.
 
 package main
@@ -19,24 +19,23 @@ func main() {
 	defer file.Close()
 
 	// WAV header
-	_ = binary.Write(file, binary.LittleEndian, []byte("RIFF"))
-	_ = binary.Write(file, binary.LittleEndian, uint32(36+sampleRate*duration)) // File size - 36 bytes (header) + sound size
-	_ = binary.Write(file, binary.LittleEndian, []byte("WAVE"))
-	_ = binary.Write(file, binary.LittleEndian, []byte("fmt "))
-	_ = binary.Write(file, binary.LittleEndian, uint32(16))
-	_ = binary.Write(file, binary.LittleEndian, uint16(1))          // audioformat (1 for PCM)
-	_ = binary.Write(file, binary.LittleEndian, uint16(1))          // Channels
-	_ = binary.Write(file, binary.LittleEndian, uint32(sampleRate)) // Sampling frequency
-	_ = binary.Write(file, binary.LittleEndian, uint32(sampleRate*1))
-	_ = binary.Write(file, binary.LittleEndian, uint16(1))
-	_ = binary.Write(file, binary.LittleEndian, uint16(8))
-	_ = binary.Write(file, binary.LittleEndian, []byte("data"))
-	_ = binary.Write(file, binary.LittleEndian, uint32(sampleRate*duration))
+	binary.Write(file, binary.LittleEndian, []byte("RIFF"))
+	binary.Write(file, binary.LittleEndian, uint32(36+sampleRate*duration)) // File size - 36 bytes (header) + sound size
+	binary.Write(file, binary.LittleEndian, []byte("WAVE"))
+	binary.Write(file, binary.LittleEndian, []byte("fmt "))
+	binary.Write(file, binary.LittleEndian, uint32(16))
+	binary.Write(file, binary.LittleEndian, uint16(1))          // audioformat (1 for PCM)
+	binary.Write(file, binary.LittleEndian, uint16(1))          // Channels
+	binary.Write(file, binary.LittleEndian, uint32(sampleRate)) // Sampling frequency
+	binary.Write(file, binary.LittleEndian, uint32(sampleRate*1))
+	binary.Write(file, binary.LittleEndian, uint16(1))
+	binary.Write(file, binary.LittleEndian, uint16(8))
+	binary.Write(file, binary.LittleEndian, []byte("data"))
+	binary.Write(file, binary.LittleEndian, uint32(sampleRate*duration))
 
 	// Генерация звука
 	for t := 0; t < sampleRate*duration; t++ {
-		val := uint8((t & int(t>>8)))
-		_ = binary.Write(file, binary.LittleEndian, val)
+		binary.Write(file, binary.LittleEndian, uint8((t & int(t>>8))))
 	}
 
 	println("Done! output.wav generated.")
